14893754765736ba566ba78331eb45df15542b98
Cl0udKiD <sasha.azarov8@gmail.com>
2023-01-02 12:33:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+   f=0;
+    if(num.toString()[0]=='-'){++f}
+    s='';
+    for(let i=num.toString().length-1;i>=0+f;--i){
+        s+=num.toString()[i];
+    }
+    return Number(s);
 }
